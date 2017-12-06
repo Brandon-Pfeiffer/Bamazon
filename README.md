0e059719a1f75a72a08cd728ea74ff4d3b67c67c
Brandon Pfeiffer <brandonpfeiffer@Brandons-MBP.attlocal.net>
2017-12-06 15:34:04 -0500

    Still Tweaking

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# bamazon
+# Bamazon
+
+##An Amazon-like storefront powered by MySQL
+
+###Functionality 
+
+! [BC](/ReadMePics/Pic1.png)
+
+! [BC](/ReadMePics/Pic2.png)
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,19 +13,74 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
 	if (err) throw err;
-	console.log("Connection successful!");
 
 	displayProducts();
 });
 
 function displayProducts() {
 	var table = new Table({
-		head: ["id item", "product name", "price"] 
+		head: ["ID", "Product Name", "Price"],
+		colWidths: [5, 25, 10]
 	});
 	connection.query("SELECT * FROM Products", function(err, res) {
 		for (var i = 0; i < res.length; i++) {
-			table.push([res[i].id_item+" || "+res[i].product_name+" || "+res[i].price+"\n"]);
+			table.push(
+				[res[i].item_id, res[i].product_name, res[i].price]
+			);
 		}
 	console.log(table.toString());	
+
+	buyProducts();
 	});
 };
+
+function buyProducts() {
+		prompt.start();
+
+		prompt.get([{
+			name: "ID",
+			message: "Whats the item ID of your product?",
+			type: "integer",
+			required: true 
+		}, {
+			name: "quantity",
+			message: "How many would you like?",
+			type: "integer",
+			required: true
+		}], function(err, answers){
+			connection.query("SELECT * FROM Products WHERE item_id=?", [answers.ID], function(err, res) {
+						var i = 0;
+						if((res[i].stock_quantity - answers.quantity) > 0){
+								var updatedQuantity = res[i].stock_quantity - answers.quantity;
+								connection.query("UPDATE Products SET stock_quantity=? WHERE item_id=?", [updatedQuantity, answers.ID], function(err, result) {});
+								var total = answers.quantity * res[i].price;
+								console.log("That'll be : $" + total.toFixed(2));
+								buyMore();
+						
+						} else {
+							console.log("Sorry, we are out of stock of " + res[i].product_name + ". Check back later!")
+							buyMore();
+						}
+				});
+		});
+}
+
+function buyMore() {
+	prompt.start();
+
+	prompt.get([{
+		name: "again",
+		message: "Would you like to buy another item? (choose y or n)",
+		required: true
+	}], function(err, answers) {
+			if (answers.again == "y" || answers.again == "Y"){
+					buyProducts()
+			} else  {
+					console.log("Have a good day.");
+					process.exit();
+			}
+	})
+}
+
+
+
